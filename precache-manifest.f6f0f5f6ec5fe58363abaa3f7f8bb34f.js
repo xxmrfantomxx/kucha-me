26f4940c53a8919e4501522453402208abeb3471
Jakub <jakub014@seznam.cz>
2019-10-28 14:34:19 +0100

    Updates

---
+++ precache-manifest.f6f0f5f6ec5fe58363abaa3f7f8bb34f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11f826d79498517d4d4cf669b01bebf9",
+    "url": "/kucha-me/index.html"
+  },
+  {
+    "revision": "7ba929f2a255694d3e0d",
+    "url": "/kucha-me/static/css/main.967ba4c2.chunk.css"
+  },
+  {
+    "revision": "9472fcca6f245ea8de92",
+    "url": "/kucha-me/static/js/2.236d3018.chunk.js"
+  },
+  {
+    "revision": "7ba929f2a255694d3e0d",
+    "url": "/kucha-me/static/js/main.bb4451c2.chunk.js"
+  },
+  {
+    "revision": "ec2703310b89696fe57c",
+    "url": "/kucha-me/static/js/runtime-main.04f17705.js"
+  }
+]);
